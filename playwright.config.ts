ea46d850b705b3a44d90fb8d4e83e3e318d4bf67
Namnung <adepratamaa99@gmail.com>
2025-04-26 12:16:25 +0700

    update

---
+++ playwright.config.ts
@@ -0,0 +1,13 @@
+import { PlaywrightTestConfig } from "playwright/test";
+const config: PlaywrightTestConfig = {
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: "on-first-retry",
+  },
+  reporter: [
+    ["html", { outputFile: "test-results.html" }], // Generates an HTML report
+  ],
+};
+export default config;
